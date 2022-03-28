bccc30a58a1b70d2ba39883d5e42e7542f1e539f
yhoogstrate <y.hoogstrate@gmail.com>
2022-03-28 17:35:54 +0200

    updates

--- R/recursiveCorPlot.R
+++ R/recursiveCorPlot.R
@@ -3,17 +3,12 @@
 # This is an example function named 'recursiveCorPlot'
 # which makes a clustered correlation plot using recursive correlation as
 # distance metric.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
 # Some useful keyboard shortcuts for package authoring:
 #
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
+#' @export
 
 
 recursiveCorPlot <- function(normalised_correlated_data, labels, font_scale , legend_scale , method="ward.D2") {
