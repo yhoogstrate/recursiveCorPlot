0cecbc1cdd049d7a2787db01f9058b08c245bf48
yhoogstrate <y.hoogstrate@gmail.com>
2022-12-16 09:26:21 +0100

    fn as by convention

---
+++ R/recursiveCorPlot.R
@@ -0,0 +1,16 @@
+#' recursiveCorPlot: natural clustering of RNA-seq data
+#'
+#' recursiveCorPlot provides two main functions:
+#' recursiveCorPlot() and geom_circle()
+#'
+#' See the vignette for more details, a step by step explanation of these functions.
+#'
+#' @docType package
+#' @name recursiveCorPlot
+#'
+#' @importFrom rlang .data
+#' @importFrom Rdpack reprompt
+#'
+NULL
+#> NULL
+
